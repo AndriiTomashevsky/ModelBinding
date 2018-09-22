f36ea51caf3348139b50764dc6071593ced17e2f
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-22 14:18:39 +0300

    Selectively Binding Properties

--- MvcModels/Controllers/HomeController.cs
+++ MvcModels/Controllers/HomeController.cs
@@ -38,7 +38,8 @@ namespace MvcModels.Controllers
             return View("Index", model);
         }
 
-        public ViewResult DisplaySummary([Bind(Prefix = nameof(Person.HomeAddress))] AddressSummary summary)
+        public ViewResult DisplaySummary(
+            [Bind(nameof(AddressSummary.City), Prefix = nameof(Person.HomeAddress))] AddressSummary summary)
         {
             return View(summary);
         }
