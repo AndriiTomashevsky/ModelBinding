66ec5063591328e74820088fee70d6cc0a087a09
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-22 14:20:20 +0300

    Selectively Binding Properties, Applying the NeverBind Attribute

--- MvcModels/Models/AddressSummary.cs
+++ MvcModels/Models/AddressSummary.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace MvcModels.Models
 {
-    [Bind(nameof(City))]
     public class AddressSummary
     {
         public string City { get; set; }
+
+        [BindNever]
         public string Country { get; set; }
     }
 }
