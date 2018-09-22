eb3a2ecc402cb703fc99efbcb12baebf76583d9e
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-22 14:30:45 +0300

    Specifying a Model Binding Source, Selecting a Standard Binding Source

--- MvcModels/Controllers/HomeController.cs
+++ MvcModels/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace MvcModels.Controllers
             repository = repo;
         }
 
-        public IActionResult Index(int? id)
+        public IActionResult Index([FromQuery] int? id)
         {
             Person person;
 
@@ -25,33 +25,6 @@ namespace MvcModels.Controllers
             {
                 return NotFound();
             }
-        }
-
-        public ViewResult Create()
-        {
-            return View(new Person());
-        }
-
-        [HttpPost]
-        public ViewResult Create(Person model)
-        {
-            return View("Index", model);
-        }
-
-        public ViewResult DisplaySummary(
-            [Bind(nameof(AddressSummary.City), Prefix = nameof(Person.HomeAddress))] AddressSummary summary)
-        {
-            return View(summary);
-        }
-
-        public ViewResult Names(IList<string> names)
-        {
-            return View(names ?? new List<string>());
-        }
-
-        public ViewResult Address(IList<AddressSummary> addresses)
-        {
-            return View(addresses ?? new List<AddressSummary>());
         }
     }
 }
